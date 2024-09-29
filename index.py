f17a0fa17e08eef4478715706a5494646b70e521
erick <erickflan10@yahoo.com>
2024-09-29 10:42:12 -0300

    tratando erro

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import JSONFormatter
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 
 # para rodar o flask -> flask --app index.py run
@@ -18,7 +18,17 @@ def index():
 
 @app.route("/transcript")
 def result():
-    transcript = YouTubeTranscriptApi.get_transcripts(["TPXr2fkz0EM"], languages=["pt"])
+    data = request.get_json()
+    video_url = data.get('url')
+    video_id = video_url.split("v=")[1]
+    try:
+        transcript = YouTubeTranscriptApi.get_transcripts([video_id], languages=["pt"])
+    except:
+        data = {
+            'Mensagem': 'Erro ao buscar o transcript'
+        }
+        return jsonify(data), 500
+    
     texto = ""
 
     formatter = JSONFormatter()
@@ -26,8 +36,8 @@ def result():
     data = json.loads(json_formatted)
 
     data_list = data[0]
-
-    texts = [item['text'] for item in data_list['TPXr2fkz0EM'] if 'text' in item]
+    
+    texts = [item['text'] for item in data_list[video_id] if 'text' in item]
 
     for text in texts:
         texto += text + " "
